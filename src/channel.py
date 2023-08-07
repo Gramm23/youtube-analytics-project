9ba5a62727af38204fa4e019a20fddcd1be95399
gramm23 <serglapinofff@gmail.com>
2023-08-07 16:41:49 +0300

    add code

--- src/channel.py
+++ src/channel.py
@@ -13,8 +13,53 @@ class Channel:
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
+        self.id = None
+        self.title = None
+        self.description = None
+        self.url = None
+        self.subscriber_count = None
+        self.video_count = None
+        self.view_count = None
+        self.update_channel_info()
+
+    @classmethod
+    def get_service(cls):
+        return youtube
+
+    def update_channel_info(self):
+        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        channel_info = channel['items'][0]
+
+        self.id = channel_info['id']
+        self.title = channel_info["snippet"]["title"]
+        self.description = channel_info["snippet"]["description"]
+        self.url = f"https://www.youtube.com/channel/{self.channel_id}"
+        self.subscriber_count = int(channel_info['statistics']['subscriberCount'])
+        self.video_count = int(channel_info['statistics']['videoCount'])
+        self.view_count = int(channel_info['statistics']['viewCount'])
+
+    def to_json(self, file_path: str) -> None:
+        data = {
+            "id": self.id,
+            "title": self.title,
+            "description": self.description,
+            "url": self.url,
+            "subscriber_count": self.subscriber_count,
+            "video_count": self.video_count,
+            "view_count": self.view_count
+        }
+
+        with open(file_path, 'w') as json_file:
+            json.dump(data, json_file, indent=2, ensure_ascii=False)
 
     def print_info(self) -> None:
-        """Выводит в консоль информацию о канале."""
-        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
-        print(json.dumps(channel, indent=2, ensure_ascii=False))
+        print(f"ID: {self.id}")
+        print(f"Название: {self.title}")
+        print(f"Описание: {self.description}")
+        print(f"Ссылка: {self.url}")
+        print(f"Подписчики: {self.subscriber_count}")
+        print(f"Видео: {self.video_count}")
+        print(f"Просмотры: {self.view_count}")
+
+
+
