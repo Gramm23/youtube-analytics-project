dcf7248b28319b44acf8d1b372a692af5134a962
gramm23 <serglapinofff@gmail.com>
2023-08-26 12:45:36 +0300

    add except for video_info

--- src/video.py
+++ src/video.py
@@ -10,30 +10,38 @@ class Video:
     def __init__(self, video_id):
         self.video_id = video_id
         self.id = None
-        self.video_title = None
+        self.title = None
         self.url_video = None
         self.view_count = None
         self.like_count = None
         self.video_info()
 
     def __str__(self):
-        return f'{self.video_title}'
+        return f'{self.title}'
 
     def video_info(self):
-        video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                               id=self.video_id
-                                               ).execute()
-        video_info = video_response['items'][0]
-        self.id = video_info['id']
-        self.video_title = str(video_info['snippet']['title'])
-        self.url_video = f"https://www.youtube.com/watch?v=jTxqXwHY-ig&list={self.id}"
-        self.view_count = int(video_info['statistics']['viewCount'])
-        self.like_count = int(video_info['statistics']['likeCount'])
+        try:
+            video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                   id=self.video_id
+                                                   ).execute()
+
+            video_info = video_response['items'][0]
+            self.id = video_info['id']
+            self.title = str(video_info['snippet']['title'])
+            self.url_video = f"https://www.youtube.com/watch?v={self.video_id}"
+            self.view_count = int(video_info['statistics']['viewCount'])
+            self.like_count = int(video_info['statistics']['likeCount'])
+        except Exception:
+            print('Введен неправильный id')
+            self.id = None
+            self.title = None
+            self.url_video = None
+            self.view_count = None
+            self.like_count = None
+
 
 
 class PLVideo(Video):
     def __init__(self, video_id, playlist_id):
         super().__init__(video_id)
         self.playlist_id = playlist_id
-
-
